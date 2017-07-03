960acfb26b985c4b996a523293910397880c9d6c
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2017-07-04 00:27:18 +0300

    fixes

--- admin.js
+++ admin.js
@@ -5,30 +5,41 @@
     var $document = $(document);
 
     var resizeOffset = function (centerX, centerY, origW, origH, destW, destH) {
-        var aspect_ratio = 1.0 * origW / origH;
+        var aspectRatio = 1.0 * origW / origH;
         var newW = Math.min(destW, origW);
         var newH = Math.min(destH, origH);
 
-        if ( ! newW ) {
-            newW = Math.round( newH * aspect_ratio );
+        if (!newW) {
+            newW = Math.round(newH * aspectRatio);
         }
 
-        if ( ! newH ) {
-            newH = Math.round( newW / aspect_ratio );
+        if (!newH) {
+            newH = Math.round(newW / aspectRatio);
         }
 
-        var size_ratio = Math.max( newW / origW, newH / origH );
+        var sizeRatio = Math.max(newW / origW, newH / origH);
 
-        var cropW = Math.round( newW / size_ratio );
-        var cropH = Math.round( newH / size_ratio );
+        var cropW = Math.round(newW / sizeRatio);
+        var cropH = Math.round(newH / sizeRatio);
 
         var sX = Math.floor(origW * centerX / 100.0 - cropW / 2.0);
         var sY = Math.floor(origH * centerY / 100.0 - cropH / 2.0);
 
-        if(sX < 0) sX = 0;
-        if(sY < 0) sY = 0;
-        if(sX > (origW - cropW)) sX = origW - cropW;
-        if(sY > (origH - cropH)) sY = origH - cropH;
+        if (sX < 0) {
+            sX = 0;
+        }
+
+        if (sY < 0) {
+            sY = 0;
+        }
+
+        if (sX > (origW - cropW)) {
+            sX = origW - cropW;
+        }
+
+        if (sY > (origH - cropH)) {
+            sY = origH - cropH;
+        }
 
         return [sX, sY];
     };
@@ -48,7 +59,7 @@
 
             separator = uri.indexOf('?') !== -1 ? '&' : '?';
 
-            return uri + separator + key + "=" + value + hash;
+            return uri + separator + key + '=' + value + hash;
         }
     };
 
@@ -59,20 +70,18 @@
 
         api.$spinner = $('#focal-spinner');
 
-        if (api.id === id) {
+        if (api.queue.hasOwnProperty(id)) {
             api.$spinner.addClass('is-active');
         } else {
-            api.id = id;
+            api.queue[id] = wp.media.attachment(id).get('meta').focalVersion;
         }
 
         api.$field = $('#attachments-' + id + '-focal-center');
-        api.cursorPos = api.$field.val().split(',').map(function(i){return parseFloat(i);});
+        api.cursorPos = api.$field.val().split(',').map(parseFloat);
 
         var updateCursorPos = function (x, y) {
-            if(x < 0) { x = 0; }
-            if(y < 0) { y = 0; }
-            if(x > 100) { x = 100; }
-            if(y > 100) { y = 100; }
+            x = Math.min(Math.max(x, 0), 100);
+            y = Math.min(Math.max(y, 0), 100);
 
             api.cursorPos = [x, y];
             $cursor.css({
@@ -81,65 +90,70 @@
             });
 
             $previews.find('li').each(function () {
-                var $img = $(this).find('img:first');
-                var offset = resizeOffset(x, y, $img.width(), $img.height(), $(this).width(), $(this).height());
+                var $el = $(this);
+                var $img = $el.find('img:first');
+                var offset = resizeOffset(x, y, $img.width(), $img.height(), $el.width(), $el.height());
+
                 $img.css({
-                    marginLeft: - offset[0],
-                    marginTop: - offset[1]
+                    marginLeft: -offset[0],
+                    marginTop: -offset[1]
                 });
             })
         };
 
         updateCursorPos(api.cursorPos[0], api.cursorPos[1]);
 
-        $selector.on('mousedown', function (e) {
-            e.preventDefault();
-            var x = e.offsetX;
-            var y = e.offsetY;
-            updateCursorPos(100.0 * x / $selector.width(), 100.0 * y / $selector.height());
-            api.dragging = true;
+        $selector.on('mousedown', function (event) {
+            event.preventDefault();
+            updateCursorPos(100.0 * event.offsetX / $selector.width(), 100.0 * event.offsetY / $selector.height());
+            api.id = id;
         });
 
-        $selector.on('mousemove', function (e) {
-            e.preventDefault();
-            if (api.dragging) {
-                var x = e.offsetX;
-                var y = e.offsetY;
-                updateCursorPos(100.0 * x / $selector.width(), 100.0 * y / $selector.height());
+        $selector.on('mousemove', function (event) {
+            event.preventDefault();
+
+            if (api.id) {
+                updateCursorPos(100.0 * event.offsetX / $selector.width(), 100.0 * event.offsetY / $selector.height());
             }
         });
     };
 
     api.id = 0;
-    api.dragging = false;
     api.saveTimer = null;
     api.$field = null;
     api.$spinner = null;
     api.cursorPos = [0, 0];
-    api.heartbeat = '';
+    api.queue = {};
 
     $document.on('mouseup', function () {
-        if(api.dragging) {
-            api.dragging = false;
-            if (api.saveTimer) {
+        var id;
+
+        if (api.id) {
+            id = api.id;
+            api.id = 0;
+
+            if (api.saveTimer !== null) {
                 clearTimeout(api.saveTimer);
             }
-            api.saveTimer = setTimeout(function() {
-                if (api.$spinner) {
+
+            api.saveTimer = setTimeout(function (id) {
+                if (api.$spinner !== null) {
                     api.$spinner.addClass('is-active');
                 }
-                if (api.$field) {
+
+                if (api.$field !== null) {
                     api.$field.val(api.cursorPos.join(',')).change();
                 }
-                api.heartbeat = 'refresh';
+
+                api.queue[id] = wp.media.attachment(id).get('meta').focalVersion;
                 wp.heartbeat.interval('fast');
-            }, 800);
+            }.bind(null, id), 800);
         }
     });
 
     $document.on('heartbeat-send', function (event, data) {
-        if (api.heartbeat !== '') {
-            data.focal_point = api.heartbeat;
+        if (!$.isEmptyObject(api.queue)) {
+            data.focal_point = $.extend({}, api.queue);
         }
     });
 
@@ -150,10 +164,7 @@
                 var editorImgSrc;
 
                 wp.media.attachment(data.id).set(data);
-
-                if (api.id === data.id) {
-                    api.id = 0;
-                }
+                delete api.queue[data.id];
 
                 if (typeof tinymce !== 'undefined' && tinymce.activeEditor && data.meta) {
                     $editorImg = tinymce.activeEditor.$('img[src^="' + data.url.replace(/\.[^.]+$/, '') + '"]');
@@ -166,10 +177,10 @@
                     }
                 }
             });
-            if (api.$spinner) {
+
+            if (api.$spinner !== null) {
                 api.$spinner.removeClass('is-active');
             }
-            api.heartbeat = '';
         }
     });
 
--- focal-point.php
+++ focal-point.php
@@ -3,10 +3,10 @@
  * Plugin Name: Thumbnails Focal Point
  */
 
-define( 'FOCAL_POINT_VERSION', '2.0.9' );
+define( 'FOCAL_POINT_VERSION', '2.0.10' );
 
 add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
-    if( empty( $form_fields ) ) {
+    if ( empty( $form_fields ) ) {
         $form_fields = [];
     }
 
@@ -20,8 +20,7 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
                     <img style=\"display: none\" src=\"{$src}\" alt=\"\" />
                     <div id=\"focal-cursor-{$post->ID}\" class=\"focal-cursor\"></div>
                     <div id=\"focal-selector-{$post->ID}\" class=\"focal-overlay\"></div>
-                </div>
-                ";
+                </div>";
 
     $html = "$selector <script>jQuery(function(){wp.media.attachment.focalPoint({$post->ID});});</script>";
 
@@ -32,7 +31,8 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
     ];
 
     $center = get_post_meta( $post->ID, 'focal-center', true );
-    if( ! $center ) {
+
+    if ( !$center ) {
         $center = '50.0,50.0';
     }
 
@@ -46,30 +46,32 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
 }, 10, 2 );
 
 add_action( 'admin_enqueue_scripts', function () {
-    wp_enqueue_script( 'focal-point-admin-js', plugin_dir_url(__FILE__) . '/admin.js', [ 'jquery' ], FOCAL_POINT_VERSION, true );
-    wp_enqueue_style( 'focal-point-admin-css', plugin_dir_url(__FILE__) . '/admin.css', FOCAL_POINT_VERSION );
+    wp_enqueue_script( 'focal-point-admin-js', plugin_dir_url( __FILE__ ) . '/admin.js', [ 'jquery' ], FOCAL_POINT_VERSION, true );
+    wp_enqueue_style( 'focal-point-admin-css', plugin_dir_url( __FILE__ ) . '/admin.css', FOCAL_POINT_VERSION );
 } );
 
 add_action( 'edit_attachment', function ( $attachment_id ) {
-    if ( isset( $_REQUEST['attachments'] ) && isset( $_REQUEST['attachments'][$attachment_id] ) && isset( $_REQUEST['attachments'][$attachment_id]['focal-center'] ) ) {
-        update_post_meta( $attachment_id, 'focal-center', $_REQUEST['attachments'][$attachment_id]['focal-center'] );
+    if ( isset( $_REQUEST['attachments'] ) && isset( $_REQUEST['attachments'][ $attachment_id ] ) && isset( $_REQUEST['attachments'][ $attachment_id ]['focal-center'] ) ) {
+        update_post_meta( $attachment_id, 'focal-center', $_REQUEST['attachments'][ $attachment_id ]['focal-center'] );
         delete_transient( 'doing_cron' );
         wp_schedule_single_event( time() - 1, 'attachment_crop', [ $attachment_id ] );
         spawn_cron();
     }
 } );
 
-add_filter( 'intermediate_image_sizes_advanced', function( $sizes, $metadata ) {
-    if( $metadata['file'] ) {
+add_filter( 'intermediate_image_sizes_advanced', function ( $sizes, $metadata ) {
+    if ( $metadata['file'] ) {
         $GLOBALS['__attachment_crop_id'] = attachment_url_to_postid( $metadata['file'] );
     }
+
     return $sizes;
 }, 10, 2 );
 
 
-add_action( 'attachment_crop', function( $attachment_id ) {
+add_action( 'attachment_crop', function ( $attachment_id ) {
     $center = get_post_meta( $attachment_id, 'focal-center', true );
     $prev_center = get_post_meta( $attachment_id, 'focal-center-prev', true );
+
     if ( $prev_center && $prev_center == $center ) {
         return;
     }
@@ -78,18 +80,22 @@ add_action( 'attachment_crop', function( $attachment_id ) {
     require_once ABSPATH . 'wp-admin/includes/image.php';
 
     $fullsizepath = get_attached_file( $attachment_id );
+
     if ( false === $fullsizepath ) {
         return;
     }
 
-    add_filter( 'intermediate_image_sizes_advanced', function( $sizes ) {
+    add_filter( 'intermediate_image_sizes_advanced', function ( $sizes ) {
         $crop_sizes = [];
-        foreach( $sizes as $name => $size ) {
-            if( ! $size['crop'] ) {
+
+        foreach ( $sizes as $name => $size ) {
+            if ( !$size['crop'] ) {
                 continue;
             }
+
             $crop_sizes[ $name ] = $size;
         }
+
         return $crop_sizes;
     } );
 
@@ -102,28 +108,21 @@ add_action( 'attachment_crop', function( $attachment_id ) {
     wp_update_attachment_metadata( $attachment_id, $metadata );
     update_post_meta( $attachment_id, 'focal-center-prev', $center );
     update_post_meta( $attachment_id, '_version', time() );
-
-    if ( false === ( $queue = get_transient( 'focal-heartbeat' ) ) ) {
-        $queue = [];
-    }
-
-    $queue[] = $attachment_id;
-    set_transient( 'focal-heartbeat', $queue, wp_nonce_tick() );
-}, 10, 1 );
+} );
 
 
 function focal_get_image_size_crop( $size ) {
-    if( is_array( $size ) ) {
+    if ( is_array( $size ) ) {
         return true; //todo: implement size array to string
     }
 
     global $_wp_additional_image_sizes;
 
-    if( in_array( $size, ['thumbnail', 'medium', 'medium_large', 'large'] ) ) {
+    if ( in_array( $size, [ 'thumbnail', 'medium', 'medium_large', 'large' ] ) ) {
         return (bool) get_option( "{$size}_crop" );
     }
 
-    if( isset( $_wp_additional_image_sizes[ $size ] ) ) {
+    if ( isset( $_wp_additional_image_sizes[ $size ] ) ) {
         return $_wp_additional_image_sizes[ $size ]['crop'];
     }
 
@@ -131,35 +130,37 @@ function focal_get_image_size_crop( $size ) {
 }
 
 
-add_filter( 'wp_get_attachment_image_src', function( $image, $attachment_id, $size ) {
-    if( $image && focal_get_image_size_crop( $size ) && ( $version = get_post_meta( $attachment_id, '_version', true ) ) ) {
+add_filter( 'wp_get_attachment_image_src', function ( $image, $attachment_id, $size ) {
+    if ( $image && focal_get_image_size_crop( $size ) && ( $version = get_post_meta( $attachment_id, '_version', true ) ) ) {
         $image[0] = add_query_arg( 'ver', $version, $image[0] );
     }
+
     return $image;
 }, 10, 3 );
 
 add_filter( 'wp_calculate_image_srcset', function( $sources, $size_array, $image_src, $image_meta, $attachment_id ) {
-    if( $version = get_post_meta( $attachment_id, '_version', true ) ) {
+    if ( $version = get_post_meta( $attachment_id, '_version', true ) ) {
         foreach( $sources as $width => $data ) {
             $sources[ $width ]['url'] = add_query_arg( 'ver', $version, $data['url'] );
         }
     }
+
     return $sources;
 }, 10, 5 );
 
 
 add_filter( 'image_resize_dimensions', function( $dimensions, $orig_w, $orig_h, $dest_w, $dest_h, $crop  ) {
-    if( ! $crop ) {
+    if ( !$crop ) {
         return $dimensions;
     }
 
-    if( ! isset( $GLOBALS['__attachment_crop_id'] ) || ! $GLOBALS['__attachment_crop_id'] || ! wp_attachment_is_image( $GLOBALS['__attachment_crop_id'] ) ) {
+    if ( !isset( $GLOBALS['__attachment_crop_id'] ) || ! $GLOBALS['__attachment_crop_id'] || ! wp_attachment_is_image( $GLOBALS['__attachment_crop_id'] ) ) {
         return $dimensions;
     }
 
     $center = get_post_meta( $GLOBALS['__attachment_crop_id'], 'focal-center', true );
 
-    if( ! $center ) {
+    if ( !$center ) {
         return $dimensions;
     }
 
@@ -169,11 +170,11 @@ add_filter( 'image_resize_dimensions', function( $dimensions, $orig_w, $orig_h, 
     $new_w = min( $dest_w, $orig_w );
     $new_h = min( $dest_h, $orig_h );
 
-    if ( ! $new_w ) {
+    if ( !$new_w ) {
         $new_w = (int) round( $new_h * $aspect_ratio );
     }
 
-    if ( ! $new_h ) {
+    if ( !$new_h ) {
         $new_h = (int) round( $new_w / $aspect_ratio );
     }
 
@@ -185,10 +186,8 @@ add_filter( 'image_resize_dimensions', function( $dimensions, $orig_w, $orig_h, 
     $s_x = floor( $orig_w * $center_x / 100.0 - $crop_w / 2.0 );
     $s_y = floor( $orig_h * $center_y / 100.0 - $crop_h / 2.0 );
 
-    if($s_x < 0) $s_x = 0;
-    if($s_y < 0) $s_y = 0;
-    if($s_x > ($orig_w - $crop_w)) $s_x = $orig_w - $crop_w;
-    if($s_y > ($orig_h - $crop_h)) $s_y = $orig_h - $crop_h;
+    $s_x = min( max( $s_x, 0 ), $orig_w - $crop_w );
+    $s_y = min( max( $s_y, 0 ), $orig_h - $crop_h );
 
     if ( $new_w >= $orig_w && $new_h >= $orig_h && $dest_w != $orig_w && $dest_h != $orig_h ) {
         return $dimensions;
@@ -221,10 +220,12 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
     $attachment_sizes = isset( $metadata['sizes'] ) ? $metadata['sizes'] : [];
 
     global $_wp_additional_image_sizes;
-    foreach( $allowed as $size ) {
-        if( isset( $_wp_additional_image_sizes[ $size ] ) && isset( $attachment_sizes[ $size ] ) ) {
+
+    foreach ( $allowed as $size ) {
+        if ( isset( $_wp_additional_image_sizes[ $size ] ) && isset( $attachment_sizes[ $size ] ) ) {
             $image_sizes = $_wp_additional_image_sizes[ $size ];
-            if( ! $image_sizes['crop'] ) {
+
+            if ( !$image_sizes['crop'] ) {
                 continue;
             }
 
@@ -233,20 +234,20 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
                 'height' => $preview_height,
                 'width' => round( $preview_height * $ratio )
             ];
-
             $duplicate_key = $preview['width'] . 'x' . $preview['height'];
-            if( in_array( $duplicate_key, $duplicates ) ) {
+
+            if ( in_array( $duplicate_key, $duplicates ) ) {
                 continue;
             }
-            $duplicates[] = $duplicate_key;
 
+            $duplicates[] = $duplicate_key;
             $source = [];
-            if( $src_ratio > $ratio ) {
+
+            if ( $src_ratio > $ratio ) {
                 //crop width
                 $source['height'] = $preview['height'];
                 $source['width'] = round( $source['height'] * $src_ratio );
-            }
-            else {
+            } else {
                 // crop height
                 $source['width'] = $preview['width'];
                 $source['height'] = round( $source['width'] / $src_ratio );
@@ -261,6 +262,7 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
     }
 
     $html_items = [];
+
     foreach( $sizes as $size ) {
         $html_items[] = "
             <li class=\"focal-preview\" style=\"width: {$size['preview']['width']}px; height: {$size['preview']['height']}px; display: none;\">
@@ -271,7 +273,7 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
     $form_fields['focal-preview'] = [
         'label' => '',
         'input' => 'html',
-        'html' => '<ul id="focal-previews" class="focal-previews">' . implode( '', $html_items ) . '</ul>',
+        'html'  => '<ul id="focal-previews" class="focal-previews">' . implode( '', $html_items ) . '</ul>',
     ];
 
     return $form_fields;
@@ -279,10 +281,26 @@ add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ) {
 
 
 add_filter( 'heartbeat_received', function ( $response, $data ) {
-    if ( isset( $data['focal_point'] ) && $data['focal_point'] == 'refresh' && false !== ( $queue = get_transient( 'focal-heartbeat' ) ) ) {
-        $response['focal_processed'] = array_map( 'wp_prepare_attachment_for_js', array_unique( $queue ) );
-        $response['heartbeat_interval'] = 'slow';
-        delete_transient( 'focal-heartbeat' );
+    if ( isset( $data['focal_point'] ) && is_array( $data['focal_point'] ) ) {
+        $processed = [];
+
+        foreach ( $data['focal_point'] as $id => $version ) {
+            if ( !isset( $processed[ $id ] ) ) {
+                $_version = get_post_meta( $id, '_version', true );
+
+                if ( $_version > $version ) {
+                    $processed[ $id ] = wp_prepare_attachment_for_js( $id );
+                }
+            }
+        }
+
+        if ( !empty( $processed ) ) {
+            $response['focal_processed'] = array_values( $processed );
+
+            if ( count( $data['focal_point'] ) == count( $processed ) ) {
+                $response['heartbeat_interval'] = 'slow';
+            }
+        }
     }
 
     return $response;
